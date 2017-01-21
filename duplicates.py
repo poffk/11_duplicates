76e70f0795ffa7a47cc9500e7e7e253a1c093322
Alexander <Alexander>
2017-01-21 12:32:34 +0300

    Рабочая версия кода, первая отправка

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,20 @@
+from os import walk
+from os.path import getsize, join
+from collections import Counter
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def get_name_size_of_all_files(path_to_folder):
+    list_of_files = []
+    for dir_path, dir_names, file_names in walk(path_to_folder):
+        for file_name in file_names:
+            list_of_files.append((file_name, getsize(join(dir_path, file_name))))
+    return list_of_files
 
 
 if __name__ == '__main__':
-    pass
+    path_start = input('Введите папку для поиска дубликатов: ')
+    list_of_files = get_name_size_of_all_files(path_start)
+    counter_files = Counter(list_of_files)
+    for file in counter_files:
+        if counter_files[file] > 1:
+                print('Дубликаты файла {0} встречаются {1} раз(а)' .format(file[0], counter_files[file]))
